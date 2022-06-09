a855831c54a5d64aab94a6deecd51f5519b03b05
Gabriel <soramoretto@gmail.com>
2022-06-09 17:07:00 -0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import { SocialList } from "./components/socialList";
+import { LinktreeHeader } from "./components/linktreeHeader";
+import { Config } from './components/config'
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="centered">
+        <div>
+          <LinktreeHeader></LinktreeHeader>
+          <SocialList></SocialList>
+        </div>
+        <Config></Config>
     </div>
   );
 }
