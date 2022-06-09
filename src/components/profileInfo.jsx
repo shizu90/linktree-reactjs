51f1ab01474e025764919f8d5c8606902c08de48
Gabriel <soramoretto@gmail.com>
2022-06-09 17:07:11 -0300

    Create profileInfo.jsx

---
+++ src/components/profileInfo.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export const ProfileInfo = (props) => {
+    return (
+        <div className="profile-info">
+            <p>{props.info}</p>
+        </div>
+    )
+}
