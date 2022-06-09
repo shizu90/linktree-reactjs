85e644b63efbf62054cd1eb5ffc150394695e00c
Gabriel <soramoretto@gmail.com>
2022-06-09 17:07:07 -0300

    Create profileImg.jsx

---
+++ src/components/profileImg.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export const ProfileImg = (props) => {
+    return (
+        <div className="profile-img">
+            <img src="https://avatars.githubusercontent.com/u/85424804?s=400&u=cc7ce2ba524d54453f2f165e939973bb349bfc0e&v=4"></img>
+        </div>
+    )
+}
