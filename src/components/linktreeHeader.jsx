dab815f44c6dd928726bddc38229c4c1ae7c2a49
Gabriel <soramoretto@gmail.com>
2022-06-10 00:40:50 -0300

    Update linktreeHeader.jsx

--- src/components/linktreeHeader.jsx
+++ src/components/linktreeHeader.jsx
@@ -1,13 +1,16 @@
-import React from "react";
+import React, { useContext } from "react";
 import { ProfileImg } from "./profileImg";
 import { ProfileInfo } from "./profileInfo";
+import {ManagerContext} from '../context/manager'
 import '../styles/linktreeHeader.css'
 
 export const LinktreeHeader = () => {
+    const context = useContext(ManagerContext)
+
     return (
         <div className="linktree-header">
             <ProfileImg></ProfileImg>
-            <ProfileInfo info = "Full stack developer"></ProfileInfo>
+            <ProfileInfo info = {context.user.length > 0 ? (context.user) : "Gabriel - gabriell#0474 - shizu90"}></ProfileInfo>
         </div>
     )
 }
