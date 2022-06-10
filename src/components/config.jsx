bb8071f781114c0f3abe61201d907ad3e7a77ec2
Gabriel <soramoretto@gmail.com>
2022-06-10 00:40:48 -0300

    Update config.jsx

--- src/components/config.jsx
+++ src/components/config.jsx
@@ -1,25 +1,65 @@
-import React from "react";
+import React, {useState, useContext} from "react";
+import { ManagerContext } from "../context/manager";
 import {MdSettings} from 'react-icons/md'
 import '../styles/config.css'
 
-const showConfig = () => {
-    
+
+const capitalize = (string) => {
+    const capitalizedString = string.charAt(0).toUpperCase() + string.slice(1).toLowerCase()
+    return capitalizedString
 }
 
-
 export const Config = () => {
+    const [opct, setOpct] = useState('none')
+    const [social, setSocial] = useState('')
+    const [url, setUrl] = useState('')
+
+    const context = useContext(ManagerContext)
+
+    const showConfig = (opct) => {
+        if(opct == 'none'){
+            setOpct('block')
+        }else{
+            setOpct('none')
+        }
+    }
+    
+    const manageUsername = (username) => {
+        if(username.length > 0){
+            context.setName(username)
+        }else{
+            return username
+        }
+    }
+
+    const manageSocialLink = (action, media, url = '') => {
+        if(media.length > 0){
+            if(action === 'add'){
+                context.setMedia({'media': capitalize(media), 'URL': url.toLowerCase(), 'add': true})
+            }else{
+                context.setMedia({'media': capitalize(media), 'URL': url.toLowerCase(), 'add': false})
+            }
+        }else{
+            console.log('Unable to add or remove undefined social media')
+        }
+            
+    }
     return (
         <div> 
             <div className="config-center">
                 <p>Settings</p>
-                <MdSettings id="configIcon"></MdSettings>
+                <MdSettings id="configIcon" onClick={() => showConfig(opct)}></MdSettings>
             </div>
-            <div className="config-page">
+            <div className="config-page" style={{display: opct}}>
                 <div className="change-propic">
                     <p className="config-title">
                         Change profile picture: 
                     </p>
-                    <input type="file"></input> 
+                    <input type="file"></input>
+                    <p className="config-title">
+                        Change username: 
+                    </p>
+                    <input type="text" onChange={(e) => {manageUsername(e.target.value)}}></input> 
                 </div>
                 <div className="socialLink-manager">
                     <p className="config-title">
@@ -28,14 +68,14 @@ export const Config = () => {
                     <div>
                         <ul>
                             <li>
-                               <input type = "text" placeholder="Social media"></input> 
+                               <input type = "text" placeholder="Social media" onChange={(e) => {setSocial(e.target.value)}}></input> 
                             </li>
                             <li>
-                                <input type = "text" placeholder="url"></input>
+                                <input type = "e" placeholder="url" onChange={(e) => {setUrl(e.target.value)}}></input>
                             </li>
                         </ul>
-                        <button>Add</button>
-                        <button>Remove</button>
+                        <button onClick={() => {manageSocialLink('add', social, url)}}>Add</button>
+                        <button onClick={() => {manageSocialLink('remove', social, url)}}>Remove</button>
                     </div>
                 </div>
             </div>
